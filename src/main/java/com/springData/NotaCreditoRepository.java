0404f5364e11c88c2dcbdb76a7ba3827bfa22de0
Hugocolman <colmanh81@gmail.com>
2025-10-23 14:59:13 -0300

    fix: reintroducir NotaCreditoRepository requerido por FacturaController

---
+++ src/main/java/com/springData/NotaCreditoRepository.java
@@ -0,0 +1,9 @@
+﻿package com.springData;
+
+import com.springData.domain.NotaCredito;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NotaCreditoRepository extends JpaRepository<NotaCredito, Long> {
+}
